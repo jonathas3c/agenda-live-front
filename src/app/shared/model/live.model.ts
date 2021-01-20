a545bf6fd40b17aeb4078922b820df378cf90939
Jonathas Germano <jonathas.germano@hotmail.com>
2021-01-20 06:21:11 -0300

    Model templates for responsePageable and Live created

---
+++ src/app/shared/model/live.model.ts
@@ -0,0 +1,11 @@
+import { StringifyOptions } from "querystring";
+
+export class Live {
+    id: String;
+    liveName: String;
+    channelName: String;
+    liveDate: String;
+    liveTime: String;
+    liveLink: String;
+    registrationDate: String;
+}
